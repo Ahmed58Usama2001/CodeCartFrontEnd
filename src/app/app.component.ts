8b36d1b279d9a36d14d4d01a829e529c6033da14
Ahmed Usama <ausama@innotech.com.eg>
2025-04-29 17:52:03 +0300

    Getting Products using TS interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from "./Layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
+import { Product } from './Shared/models/Product';
+import { Pagination } from './Shared/models/PAgination';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,9 +17,11 @@ export class AppComponent implements OnInit {
 
   private http = inject(HttpClient)
 
+  products:Product[] =[] 
+
   ngOnInit(): void {
-    this.http.get(this.baseUrl + 'products').subscribe({
-      next:data=>console.log(data),
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next:response=>this.products=response.data,
       error: error=>console.log(error),
       complete: ()=>console.log('completed')
     })

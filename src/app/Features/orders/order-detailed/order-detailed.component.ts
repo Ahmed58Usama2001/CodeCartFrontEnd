fe194db82d254d8bd3fad38a8d7960224530b724
Ahmed Usama <ausama@innotech.com.eg>
2025-06-26 12:06:21 +0300

    order component template and design

--- src/app/Features/orders/order-detailed/order-detailed.component.ts
+++ src/app/Features/orders/order-detailed/order-detailed.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../Core/services/order.service';
+import { SnackbarService } from '../../../Core/services/snackbar.service';
+import { Order } from '../../../Shared/models/Order';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-detailed',
@@ -6,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './order-detailed.component.html',
   styleUrl: './order-detailed.component.css'
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  private orderService = inject(OrderService);
+  private snackBarService = inject(SnackbarService);
+  private activatedRoute = inject(ActivatedRoute);
+
+  order?: Order;
+
+
+  ngOnInit(): void {
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) {
+      this.snackBarService.error('Order ID is missing');
+      return
+    }
+
+    this.orderService.GetOrderDetailed(+id).subscribe({
+      next: (order) => {
+        this.order = order;
+      },
+      error: (error) => {
+        this.snackBarService.error(error);
+      }
+    })
+  }
 
 }
--- src/app/Features/orders/order/order.component.ts
+++ src/app/Features/orders/order/order.component.ts
@@ -1,29 +1,60 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { OrderService } from '../../../Core/services/order.service';
 import { Order } from '../../../Shared/models/Order';
 import { SnackbarService } from '../../../Core/services/snackbar.service';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  standalone: true,
+  imports: [
+    CurrencyPipe, 
+    DatePipe, 
+    RouterLink,
+    MatTableModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatChipsModule
+  ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
 export class OrderComponent implements OnInit {
   private orderService = inject(OrderService);
   private snackBarService = inject(SnackbarService);
-  orders:Order[] = [];
+  
+  orders: Order[] = [];
+  loading = true;
+  displayedColumns: string[] = ['id', 'orderDate', 'total', 'orderStatus', 'deliveryMethod'];
 
   ngOnInit(): void {
     this.orderService.GetOrdersForUser().subscribe({
       next: (orders) => {
         this.orders = orders;
+        this.loading = false;
       },
       error: (error) => {
         this.snackBarService.error(error);
+        this.loading = false;
       }
     });
   }
 
-
+  getStatusColor(status: string): string {
+    switch (status.toLowerCase()) {
+      case 'pending':
+        return 'warn';
+      case 'PaymentReceived':
+        return 'primary';
+      case 'PaymentFailed':
+        return 'accent';
+      default:
+        return 'primary';
+    }
+  }
 }

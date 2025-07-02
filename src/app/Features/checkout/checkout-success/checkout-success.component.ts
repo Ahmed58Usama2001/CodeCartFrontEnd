e64f1b5db2f956c9cada1ba256f63a1cdd2d4275
Ahmed Usama <ausama@innotech.com.eg>
2025-07-02 17:05:15 +0300

    implementing the checkout sucess page

--- src/app/Features/checkout/checkout-success/checkout-success.component.ts
+++ src/app/Features/checkout/checkout-success/checkout-success.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SignalrService } from '../../../Core/services/signalr.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
+import { AddressPipe } from '../../../Shared/pipes/address.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-success',
+   imports: [CommonModule,
+    MatProgressSpinnerModule,  
+    MatIconModule,             
+    MatButtonModule, 
+    CurrencyPipe,
+    DatePipe        ,
+    AddressPipe  ,
+    RouterLink
+  ],
   standalone: true,
-  imports: [],
   templateUrl: './checkout-success.component.html',
-  styleUrl: './checkout-success.component.css'
+  styleUrls: ['./checkout-success.component.css']
 })
 export class CheckoutSuccessComponent {
 
+  public signalrService = inject(SignalrService);
+  
 }
